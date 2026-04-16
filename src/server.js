aefbdb1c1aa94eb5eb108f6f11214df253e21f0e
Manikanta <manikantam@techmologics.com-at-287685630918>
2026-04-16 16:32:46 +0530

    Stabilize backend git setup and update allowed frontend origin

--- src/server.js
+++ src/server.js
@@ -36,6 +36,7 @@ const allowedOrigins = new Set(
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://bulkmailsending-frontend.vercel.app/",
   ].filter(Boolean)
 );
 
